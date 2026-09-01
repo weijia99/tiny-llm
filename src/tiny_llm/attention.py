918be69ea4418d9a3ade770a35108ade72c65acb
weijia99 <646025053@qq.com>
2026-09-01 22:52:52 +0800

    Implement scaled dot product attention and causal mask; initialize Qwen3MultiHeadAttention parameters

--- src/tiny_llm/attention.py
+++ src/tiny_llm/attention.py
@@ -61,7 +61,15 @@ class SimpleMultiHeadAttention:
 
 
 def causal_mask(L: int, S: int, dtype: mx.Dtype) -> mx.array:
-    pass
+    # mask = mx.full((L,S), -mx.inf, dtype=dtype)
+    # dis = S - L if S > L else 0
+    # for i in range(L):
+    #     for j in range(dis+i+1):
+    #         mask[i,j] = 0
+    # return mask
+    # 生成下三角矩阵
+    dis = S - L if S > L else 0
+    return mx.triu(mx.full((L, S), -mx.inf, dtype=dtype), k=dis + 1)
 
 
 def scaled_dot_product_attention_grouped(
@@ -71,7 +79,32 @@ def scaled_dot_product_attention_grouped(
     scale: float | None = None,
     mask: mx.array | str | None = None,
 ) -> mx.array:
-    pass
+    # 抽取出hidden和repeat的维度
+    query_head = query.shape[-3]
+    key_head = key.shape[-3]
+    n_repeat = query_head // key_head
+    # 计算缩放因子
+    factor = mx.rsqrt(query.shape[-1]) if scale is None else scale
+    query = query.reshape(*query.shape[:-3], key_head, n_repeat,query.shape[-2], query.shape[-1])
+    key = key.reshape(*key.shape[:-3], key_head, 1, key.shape[-2], key.shape[-1])
+    value = value.reshape(*value.shape[:-3], key_head, 1, value.shape[-2], value.shape[-1])
+    # 计算qkt
+    hidden = mx.matmul(query, key.swapaxes(-2, -1)) * factor
+    # 处理mask
+    if mask is not None:
+        if isinstance(mask, str) and mask == "causal":
+            mask = causal_mask(hidden.shape[-2], hidden.shape[-1], hidden.dtype)
+            hidden = hidden + mask
+        else:
+            mask = mask.reshape(*hidden.shape[:-4], key_head, n_repeat, hidden.shape[-2], hidden.shape[-1])
+            hidden = hidden + mask
+    # 计算softmax
+    hidden = softmax(hidden, axis=-1)
+    # 计算qktv
+    hidden = mx.matmul(hidden, value)
+    # 恢复原来的维度
+    hidden = hidden.reshape(*query.shape[:-4], query_head, hidden.shape[-2], hidden.shape[-1])
+    return hidden
 
 
 def paged_attention(
--- src/tiny_llm/qwen3_week1.py
+++ src/tiny_llm/qwen3_week1.py
@@ -25,14 +25,43 @@ class Qwen3MultiHeadAttention:
         theta: int = 1000000,
         rms_norm_eps: float = 1e-5,
     ):
-        pass
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.head_dim = head_dim
+        self.wq = wq
+        self.wk = wk
+        self.wv = wv
+        self.wo = wo
+        self.q_norm = q_norm
+        self.k_norm = k_norm
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+        self.rms_norm_eps = rms_norm_eps
 
     def __call__(
         self,
         x: mx.array,
         mask: mx.array | str | None = None,
     ) -> mx.array:
-        pass
+        # 最终是head，L，dimension
+        q = linear(x, self.wq).reshape(*x.shape[:-1], self.num_heads, self.head_dim)
+        k = linear(x, self.wk).reshape(*x.shape[:-1], self.num_kv_heads, self.head_dim)
+        v = linear(x, self.wv).reshape(*x.shape[:-1], self.num_kv_heads, self.head_dim)
+        q = mx.fast.rms_norm(q,self.q_norm,self.rms_norm_eps)
+        k = mx.fast.rms_norm(k,self.k_norm,self.rms_norm_eps)
+        rope = RoPE(self.head_dim, self.max_seq_len, self.theta)
+        q = rope(q, slice(0, x.shape[-2]))   # 应用，不是构造
+        k = rope(k, slice(0, x.shape[-2]))
+        q = q.swapaxes(-2, -3)  # (batch, head, seq_len, head_dim)
+        k = k.swapaxes(-2, -3)  # (batch, head,
+        v = v.swapaxes(-2, -3)  # (batch, head, seq_len, head_dim)
+        output = scaled_dot_product_attention_grouped(
+            q, k, v, mask=mask
+        ).swapaxes(-2, -3).reshape(*x.shape[:-1], self.hidden_size)
+        output = linear(output, self.wo)
+        return output
+
 
 
 class Qwen3MLP:
@@ -44,7 +73,11 @@ class Qwen3MLP:
         w_up: mx.array,
         w_down: mx.array,
     ):
-        pass
+        self.dim = dim
+        self.hidden_dim = hidden_dim
+        self.w_gate = w_gate
+        self.w_up = w_up
+        self.w_down = w_down
 
     def __call__(self, x: mx.array) -> mx.array:
         pass
