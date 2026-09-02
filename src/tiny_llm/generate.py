ba8b0427f888282180f92a9cf7280625d0b60c6a
weijia99 <646025053@qq.com>
2026-09-02 23:57:26 +0800

    Implement simple_generate function with sampling and token generation logic

--- src/tiny_llm/generate.py
+++ src/tiny_llm/generate.py
@@ -19,8 +19,30 @@ def simple_generate(
     sampler: Callable[[mx.array], mx.array] | None,
 ) -> None:
     def _step(model, y):
-        pass
+        
+        logits = model(y[None])
+        logits = logits[:, -1, :]
+        logprobs = logits - mx.logsumexp(logits, keepdims=True)
+        if sampler is not None:
+            y = sampler(logprobs)
+        else:
+            y = mx.argmax(logprobs, axis=-1)
+        return y
+    
+    tokens = mx.array(tokenizer.encode(prompt, add_special_tokens=False))
+    detokenizer = tokenizer.detokenizer
+    detokenizer.reset()
 
+    
+    while True:
+            token = _step(model, tokens)
+            mx.eval(token)
+            tokens = mx.concat([tokens, token])
+            if token.item() == tokenizer.eos_token_id:
+                break
+            detokenizer.add_token(token.item())
+            print(detokenizer.last_segment, end="", flush=True)
+    
 
 def simple_generate_with_kv_cache(
     model: Qwen3ModelWeek2, tokenizer: TokenizerWrapper, prompt: str
