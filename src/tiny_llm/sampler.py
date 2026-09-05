f3328c9565d854f16643626b4ef4c295fa01cadc
weijia99 <646025053@qq.com>
2026-09-05 11:32:11 +0800

    Implement sampling logic in make_sampler function with top-k and top-p filtering

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -6,6 +6,19 @@ def make_sampler(temp: float, top_p: float | None, top_k: int | None):
     def sample(logprobs: mx.array):
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
-        pass
-
+        logprobs = copy.copy(logprobs)  # TODO: do we really need a copy?
+        if top_k is not None and top_k > 0:
+            mask_elements = mx.argpartition(-logprobs, kth=top_k - 1, axis=-1)[
+                :, top_k:
+            ]
+            logprobs[:, mask_elements] = -mx.inf
+        if top_p is not None and top_p > 0:
+            sorted_idx = mx.argsort(-logprobs, axis=-1)
+            sorted_logprobs = logprobs[:, sorted_idx]
+            sorted_probs = mx.exp(sorted_logprobs)
+            cumsum = mx.cumsum(sorted_probs, axis=-1)
+            mask_elements = cumsum - sorted_probs < top_p
+            logprobs[:, sorted_idx] = mx.where(mask_elements, sorted_logprobs, -mx.inf)
+        logprobs = logprobs / temp
+        return mx.random.categorical(logprobs, axis=-1)
     return sample
